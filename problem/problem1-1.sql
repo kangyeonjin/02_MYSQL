accacf713ae4553ec123309178613dc42d39cd29
강연진 <yeonjin917n.n@gmail.com>
2024-06-10 17:06:09 +0900

    problem1-1

---
+++ problem/problem1-1.sql
@@ -0,0 +1,32 @@
+-- DEPARTMENT 테이블에서 모든 컬럼을 조회하시오.
+
+    select *
+    from department;
+
+-- EMPLOYEE 테이블에서 EMP_NAME과 SALARY 컬럼을 조회하시오.
+
+    select
+        EMP_NAME,
+        SALARY
+    from
+        employee;
+
+-- JOB 테이블에서 JOB_CODE와 JOB_NAME을 조회하시오.
+
+    select
+        JOB_CODE,
+        JOB_NAME
+    from
+        job;
+
+-- LOCATION 테이블에서 LOCAL_CODE와 LOCAL_NAME을 조회하시오.
+
+    select
+        LOCAL_CODE,
+        LOCAL_NAME
+    from
+        location
+
+-- NATION 테이블에서 NATIONAL_NAME의 별칭을 '국가명'으로 하여 조회하시오
+    select
+        concat('국가명');
